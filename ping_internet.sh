228e2700d905a5bac6f43c43fbca7dae3ebd5ccb
Gregory DAVID <gregory.david@p1sec.com>
2024-01-24 10:26:39 +0100

    add external IP

--- ping_internet.sh
+++ ping_internet.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo -n 
 if ping -n -w 1 -c 1 8.8.8.8 &>/dev/null ;
 then
-    echo 🟢
+    echo $(wget -qO - icanhazip.com) 🟢 && echo 🟢 && echo '#ff00ff'
 else
-    echo 🔴
+    echo 🔴 && echo 🔴 && echo '#ff0000'
 fi
